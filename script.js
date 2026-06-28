dd95b01ac37e1116bf6dca0dcd7e90a3e8cee0df
Sivavempuluru14 <sivavempuluru12@gmail.com>
2026-06-28 13:39:04 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,70 +1,4 @@
-function login() {
-
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    let msg = document.getElementById("msg");
-
-    if (!email || !password) {
-        msg.style.color = "red";
-        msg.innerHTML = "Pfunction login() {
-
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    let msg = document.getElementById("msg");
-
-    if (!email || !password) {
-        msg.style.color = "red";
-        msg.innerHTML = "Please fill all fields.";
-        return;
-    }
-
-    // Get registered users
-    let users = JSON.parse(localStorage.getItem("users")) || [];
-
-    // Check whether email is registered
-    let registeredUser = users.find(u => u.email === email);
-
-    if (!registeredUser) {
-        msg.style.color = "red";
-        msg.innerHTML = "Email register avvaledu. Mundhu Register avvandi!";
-        return;
-    }
-
-    // Check password
-    if (registeredUser.password === password) {
-
-        msg.style.color = "green";
-        msg.innerHTML = "Login Successful!";
-
-        // Store logged in user
-        localStorage.setItem("loggedInUser", JSON.stringify(registeredUser));
-
-        // Redirect to Home Page
-        setTimeout(() => {
-            window.location.href = "home.html";
-        }, 1000);
-
-    } else {
-
-        msg.style.color = "red";
-        msg.innerHTML = "Incorrect Password";
-
-    }
-}lease fill all fields.";
-        return;
-    }
-
-    // Get registered users
-    let users = JSON.parse(localStorage.getItem("users")) || [];
-
-    // Check whether email is registered
-    let registeredUser = users.find(u => u.email === email);
-
-    if (!registeredUser) {
-        msg.style.color = "red";
-        msg.innerHTML = "Email is not registered. Please register first";
-        return;
-    }function register(){
+function register(){
 
   let name = document.getElementById("name").value;
   let email = document.getElementById("email").value;
@@ -175,28 +109,6 @@ function login() {
         msg.innerHTML = "Email is not registered. Please register first";
         return;
     }
-
-    // Check password
-    if (registeredUser.password === password) {
-
-        msg.style.color = "green";
-        msg.innerHTML = "Login Successful!";
-
-        // Store logged in user
-        localStorage.setItem("loggedInUser", JSON.stringify(registeredUser));
-
-        // Redirect to Home Page
-        setTimeout(() => {
-            window.location.href = "home.html";
-        }, 1000);
-
-    } else {
-
-        msg.style.color = "red";
-        msg.innerHTML = "Incorrect Password";
-
-    }
-}
 
     // Check password
     if (registeredUser.password === password) {
